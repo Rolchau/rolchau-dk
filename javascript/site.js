c386c93bcca361fcc0a59d6ab95cc63f13db4996
Rolchau <rolchau@gmail.com>
2016-06-04 21:34:56 +0200

    Added animation for menu

--- javascript/site.js
+++ javascript/site.js
@@ -2,7 +2,8 @@ var ROLCHAU = {};
 
 (function() {
   var sliderItem,
-      siteNavBar;
+      siteNavBar,
+      aItem;
 
   function onClick(e) {
     var target = e.target;
@@ -10,26 +11,39 @@ var ROLCHAU = {};
     if (target === e.currentTarget) {
       return;
     } else {
-      e.currentTarget.querySelector('.is-active').classList.remove('is-active');
+      aItem.classList.remove('is-active');
     }
 
     if (target.classList.contains('site-header__nav-item')) {
       target = target.parentNode;
     }
 
-    console.log(target);
+    sliderItem.style.transform = getSlideToStyle(target);
+
+  }
+
+  function getSlideToStyle(toElm) {
+    let bounds = toElm.getBoundingClientRect();
+    return 'translateX(' + (bounds.left) + 'px) translateY(' + bounds.top + 'px) scale3d(' + bounds.width / 200 + ', 1, 1)';
   }
 
   function init() {
     sliderItem = document.createElement('div');
     sliderItem.classList.add('slider-item');
+
+    aItem = document.querySelector('.is-active');
+    sliderItem.style.transform = getSlideToStyle(aItem);
+
     siteNavBar = document.querySelector('.site-header__nav');
     siteNavBar.addEventListener('click', onClick);
     siteNavBar.appendChild(sliderItem);
   }
 
+
+
   ROLCHAU.menuSlider = {
-    init: init
+    init: init,
+    slideTo: getSlideToStyle
   };
 }());
 
